b8b18939e12ac0e9f6d3cafc35db73fd7b54635f
Dakhnovskaya Olga <lokomoivvv@bk.ru>
2018-10-01 21:44:48 +0400

    Добавлено отображение

--- tenders_api/views.py
+++ tenders_api/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Tender
+
 
 def tender_list(request):
-    return render(request, 'tenders_api/tender_list.html', {})
+    tenders = Tender.objects.all()
+    return render(request, 'tenders_api/tender_list.html', {'tenders': tenders})
