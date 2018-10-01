802fd6f073e00f0f0c167a88b3c6ffe3b3fca6c7
Dakhnovskaya Olga <lokomoivvv@bk.ru>
2018-10-01 20:05:35 +0400

    Добавлены модели

--- tenders_api/models.py
+++ tenders_api/models.py
@@ -1,14 +1,33 @@
 from django.db import models
+from django.utils import timezone
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Наименование заказчика')
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Наименование категории')
+
+
+class Stage(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Наименование этапа')
+
+
+class TypePurchase(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Наименование вида')
 
 
 class Tender(models.Model):
-    date_sum_up = models.DateField(verbose_name='Дата принятия решения по тендеру')
-    cost_item = models.CharField(max_length=100,verbose_name='Статья расходов')
-    item = models.CharField(max_length=200, verbose_name='Предмет тендера')
-    customer = models.CharField(max_length=100, verbose_name='Заказчик')
-    contractor = models.CharField(max_length=100, verbose_name='Исполнитель')
-    period_validator = models.DurationField(verbose_name='Период, на который действуют итоги тендера')
-    total_amount = models.FloatField(verbose_name='Итоговая сумма торгов')
-    not_carried = models.BooleanField(null=True)
-    level_of = models.CharField(max_length=100)
-    order_selection_win = models.CharField(max_length=500)
+    deadline_app = models.DateTimeField(verbose_name='Приём заявок закончится')
+    date_publication = models.DateTimeField(default=timezone.now, verbose_name='Дата публикации')
+    customer = models.ForeignKey(Customer, verbose_name='Заказчик', on_delete=models.PROTECT)
+    brief_description = models.CharField(max_length=256, verbose_name='Краткое описание')
+    initial_price = models.FloatField(verbose_name='Начальная цена')
+    contract_guarantee = models.FloatField(verbose_name='Обеспечение контракта')
+    place_performance = models.CharField(max_length=512, verbose_name='Место исполнения')
+    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.PROTECT)
+    procurement_stage = models.ForeignKey(Stage, verbose_name='Этап закупки', on_delete=models.PROTECT)
+    type_purchase = models.ForeignKey(TypePurchase, verbose_name='Вид закупки', on_delete=models.PROTECT)
+
+
